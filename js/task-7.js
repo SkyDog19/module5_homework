bcd0cf00f946c4ff4871a1b0b5b352bf0119fe80
SkyDog19 <98251294+SkyDog19@users.noreply.github.com>
2022-12-23 01:12:35 +0300

    Update task-7.js

--- js/task-7.js
+++ js/task-7.js
@@ -1,12 +1,12 @@
 'use strict';
 /*jshint -W097*/
 
-const arr = [1, 5, 4, 6, 9, 0, 'string'];
+const arr = [1, 5, 4, 6, 9, 0, 'string', 'string-two'];
 
 let even = [];
 let odd = [];
 let zero = [];
-
+let notNumbers = [];
 
 arr.forEach(item => {
     if (item === 0) {
@@ -15,7 +15,9 @@ arr.forEach(item => {
         odd.push(item);
     } else if (item % 2 != 0 && item === Number(item)) {
         even.push(item);
+    } else if (item !== Number(item)) {
+        notNumbers.push(item);
     }
 });
 
-console.log(`Количество четных символов : ${even.length}, количество нечетных символов : ${odd.length}, количество нулей : ${zero.length}`);
+console.log(`Количество четных символов : ${even.length}, количество нечетных символов : ${odd.length}, количество нулей : ${zero.length}, количество строк ${notNumbers.length}`);
